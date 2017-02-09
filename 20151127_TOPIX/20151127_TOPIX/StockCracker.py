21ca374554144fd0720546b5a57c60f12f3f64f8
Ubuntu@Work <chen.feiyu@jp.panasonic.com>
2017-02-09 19:08:15 +0900

    20170209 Upload VisualStudioProject from office, Panasonic, Yokohama

---
+++ 20151127_TOPIX/20151127_TOPIX/StockCracker.py
@@ -0,0 +1,79 @@
+﻿import pandas as pd
+import numpy as np
+import matplotlib
+import datetime
+from matplotlib import pyplot as plt
+import seaborn as sns
+import os
+sns.set(style='ticks')
+
+from colorama import init
+init(autoreset=True)
+from colorama import Fore, Back, Style
+
+class StockCracker:
+    def __init__(self):
+        self.rootPath = './'
+        self.USDJPYPath = './fx/USDJPY/'
+        self.mazdaPath = './Mazda/'
+        self.nikkeiPath = './Nikkei/'
+
+    # read one csv file
+    def readOneCsv(self, filePath, isReversed):
+        data = pd.read_csv(filePath, encoding='shift-JIS', header=0)
+        if isReversed is True:
+            data = data.reindex(index=data.index[::-1])
+            data.index = range(len(data))
+        print(Fore.YELLOW + data['日付'][0] + '~' + data['日付'][len(data)-1])
+        return data
+
+    # read one folder, recurssively
+    def readOneFolder(self, folderPath, isReversed=True):
+        for root, subdirs, files in os.walk(folderPath):
+            print('----------------')
+            print(Back.CYAN + Fore.RED + Style.BRIGHT + 
+                  'In Folder:'+root)
+            for oneCsv in files:
+                print(Back.CYAN + oneCsv)
+                filePath = folderPath+oneCsv
+                data = self.readOneCsv(filePath, isReversed)
+                try:
+                    allData
+                except NameError:   # allData not exist
+                    allData = data
+                else:   # allData exist
+                    # concatenate 2 pandas dataframes
+                    allData = pd.concat([allData, data])
+        allData.index = range(len(allData))
+        #print(allData) # output all data of target stock
+        return allData
+
+if __name__ == '__main__':
+
+    sc = StockCracker()
+    # reading data ------------------------------------
+    data_Nikkei = sc.readOneFolder(sc.nikkeiPath)
+    data_Mazda  = sc.readOneFolder(sc.mazdaPath)
+    data_fx     = sc.readOneFolder(sc.USDJPYPath, False)
+    ## convert date
+    date_Mazda    = [datetime.datetime.strptime(d, '%Y-%m-%d').date() for d in data_Mazda['日付'][:]]
+    date_Nikkei   = [datetime.datetime.strptime(d, '%Y-%m-%d').date() for d in data_Nikkei['日付'][:]]
+    date_fx       = [datetime.datetime.strptime(d, '%Y/%m/%d').date() for d in data_fx['日付'][:]]
+    ### for Mazda, 2014-07-29 株併合:1株 -> 0.2株
+    index = data_Mazda.loc[data_Mazda['日付'] == '2014-07-28'].index[0]
+    data_Mazda['始値'][0:(index+1)] = data_Mazda['始値'][0:(index+1)] * 5
+    
+    # plot data ----------------------------------------
+    fig, (ax, bx) = plt.subplots(2, sharex = True)
+    ## Nikkei
+    ax.plot(date_Nikkei, data_Nikkei['始値'][:], c='r')
+    ax.set_ylabel('Nikkei', color='r', fontsize=20)
+    ## Mazda
+    ax1 = ax.twinx()
+    ax1.plot(date_Mazda, data_Mazda['始値'][:], c='b')
+    ax1.set_ylabel('Mazda', color='b', fontsize=20)
+    ## fx USDJPY
+    bx.plot(date_fx, data_fx['始値'][:], c='c')
+    bx.set_ylabel('USDJPY', color='c', fontsize=20)
+
+    plt.show()
---
+++ 20151127_TOPIX/20151127_TOPIX/_20151127_TOPIX.py
@@ -0,0 +1,143 @@
+﻿import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+import numpy as np
+import matplotlib.mlab as mlab
+import math
+import matplotlib
+import csv
+import os.path
+import datetime
+# refer to the example in scikit-learn webpage!!!!
+import sklearn 
+
+rootpath = './'
+
+def readCSV(fileName):
+    file_path = os.path.join(rootpath, fileName)
+    file = open(file_path)
+    data = csv.reader(file, delimiter=',')
+    return data
+
+data2013 = readCSV('I102_2013.csv')
+data2014 = readCSV('I102_2014.csv')
+data2015 = readCSV('I102_2015.csv')
+
+#####################################
+# all data store in these arrays
+dates   = []
+starts  = []
+highs   = []
+lows    = []
+ends    = []
+#-----------------------------------#
+
+def readData(dataName):
+    dates_      =[]
+    starts_     =[]
+    highs_      =[]
+    lows_       =[]
+    ends_       =[]
+    for row in dataName:
+        date    = row[0]
+        start   = row[1]
+        high    = row[2]
+        low     = row[3]
+        end     = row[4]
+
+        dates_.append(date)
+        starts_.append(start)
+        highs_.append(high)
+        lows_.append(low)
+        ends_.append(end)
+
+    # setting the data chronicle
+    dates_.reverse()
+    starts_.reverse()
+    highs_.reverse()
+    lows_.reverse()
+    ends_.reverse()
+
+    dates.extend( dates_ )
+    starts.extend( starts_ )
+    highs.extend( highs_ )
+    lows.extend( lows_ )
+    ends.extend( ends_ )
+    return
+
+readData(data2013)
+readData(data2014)
+readData(data2015)
+
+#################################
+#   detect changing point
+avg_before      = []
+avg_after       = []
+avg_around      = []
+#   fill in the 3 lists
+def detectChange(data_string):
+    dataList = []
+    for element in data_string:
+        dataList.append( float(element) )
+    boundry_before = 0
+    boundry_after = 0
+    for index in range(len(lows)):
+        # decide upper boundry
+        if index - 100 < 0:
+            boundry_before = 0;
+        else:
+            boundry_before = index -100
+        # decide lower boundry
+        if index + 100 > len(lows):
+            boundry_after = len(lows)
+        else:
+            boundry_after = index + 100
+
+        avg_before.append( np.average(dataList[boundry_before:index]) )
+        avg_after.append( np.average(dataList[index:boundry_after]) )
+        avg_around.append( np.average(dataList[boundry_before:boundry_after]) )
+    return
+#--------------------------------#
+
+detectChange(lows)
+
+##############################
+#   calculate weight
+weights = []
+def calcWeights(data_string):
+    dataList = []
+    for element in data_string:
+        dataList.append( float(element) )
+
+    for index in range(len(dataList)):
+        if index == 0:
+            weights.append(0)
+        else:
+            weights.append(dataList[index] - avg_before[index] + avg_around[index] -avg_after[index])
+    return
+#-----------------------------#
+calcWeights(lows)
+
+for index in range(len(weights)):
+    weights[index] += math.fabs(min(weights))
+
+###############################
+#   make a-axis display date
+x = [datetime.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
+#-----------------------------#
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.gca().xaxis.set_visible(False)
+plt.plot(x, starts,     label='starts')
+plt.plot(x, highs,      label='highs')
+plt.plot(x, lows,       label='lows')
+plt.plot(x, ends,       label='ends')
+plt.plot(x, weights,    label='weights')
+plt.gcf().autofmt_xdate()
+plt.title('TOPIX Analysis')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend(loc=5)
+plt.grid()
+plt.show()
