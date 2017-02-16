dceac7765aae9b77671205b8ef0900929fd0a048
legend507 <chenfeiyu507@gmail.com>
2017-02-16 12:13:21 +0900

    add iPython

--- 20151127_TOPIX/20151127_TOPIX/StockCracker.py
+++ 20151127_TOPIX/20151127_TOPIX/StockCracker.py
@@ -1,10 +1,10 @@
-﻿import pandas as pd
+import pandas as pd
 import numpy as np
-import matplotlib
 import datetime
+import os
+import matplotlib
 from matplotlib import pyplot as plt
 import seaborn as sns
-import os
 sns.set(style='ticks')
 
 from colorama import init
