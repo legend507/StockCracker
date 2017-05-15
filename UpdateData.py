6dc58a4d0a9e4a9e10089edbe65981a1abd50e37
legend507 <chenfeiyu507@gmail.com>
2017-05-15 13:28:49 +0900

    finishes Update Data script, still need to add proxy

--- UpdateData.py
+++ UpdateData.py
@@ -2,7 +2,6 @@ import httplib2
 h = httplib2.Http()
 resp, content = h.request("http://k-db.com/indices/I101/1d/2017?download=csv", "GET")
 
-
-oFile = open('./test.csv', 'w')
+oFile = open('./test.csv', 'wb')
 oFile.write(content)
 oFile.close()
