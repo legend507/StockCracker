6d58eeb0713cea69ea65af93f8dcd41fc518aa08
legend507Work <chenfeiyu507@gmail.com>
2017-05-15 14:13:24 +0900

    try to add proxy to UpdateData.py, failed

--- UpdateData.py
+++ UpdateData.py
@@ -1,6 +1,16 @@
 import httplib2
-h = httplib2.Http()
-resp, content = h.request("http://k-db.com/indices/I101/1d/2017?download=csv", "GET")
+import sys
+
+try:
+    h = httplib2.Http()    
+    resp, content = h.request("http://k-db.com/indices/I101/1d/2017?download=csv", "GET")
+except:
+    print("Using Proxy")
+    h = httplib2.Http(
+        proxy_info = httplib2.ProxyInfo(
+            httplib2.socks.PROXY_TYPE_HTTP, 'proxy.mei.co.jp', 8080
+            ) )
+    resp, content = h.request("http://k-db.com/indices/I101/1d/2017?download=csv", "GET")
 
 oFile = open('./test.csv', 'wb')
 oFile.write(content)
