9cd696f434b98055bc619393b1aa8617ece8b91c
legend507Work <chenfeiyu507@gmail.com>
2017-05-15 13:19:18 +0900

    update UpdateData.py script

--- UpdateData.py
+++ UpdateData.py
@@ -1,5 +1,8 @@
 import httplib2
-h = httplib2.Http(".cache")
+h = httplib2.Http()
 resp, content = h.request("http://k-db.com/indices/I101/1d/2017?download=csv", "GET")
 
-print(content)
+
+oFile = open('./test.csv', 'w')
+oFile.write(content)
+oFile.close()
