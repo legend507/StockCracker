bf21c8de8657bccf4d7c5bf5bfb56b8ef918339a
legend507 <chenfeiyu507@gmail.com>
2017-12-27 16:29:01 +0900

    Still trying to get Bitcoin data

--- BTCtest.py
+++ BTCtest.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from datetime import datetime
 
-df = pd.read_csv("http://api.bitcoincharts.com/v1/trades.csv?symbol=coincheckJPY",
+df = pd.read_csv("./20151127_TOPIX/20151127_TOPIX/Bitcoin/test.csv",
                  header=None,
                  parse_dates=True,
                  date_parser=lambda x: datetime.fromtimestamp(float(x)),
                  index_col='datetime',
                  names=['datetime', 'price', 'amount'])
 df["price"].plot()
+
+df.to_csv('./20151127_TOPIX/20151127_TOPIX/Bitcoin/BTC.csv')
