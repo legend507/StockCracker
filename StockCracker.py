1aaea68ca69da32f140c3753b7d5e83e15df4ff6
CHEN <chen@ubuntu.ubuntu-domain>
2019-04-30 08:51:38 +0900

    rewrote script using classes. restructured script and notebook.

---
+++ StockCracker.py
@@ -0,0 +1,202 @@
+import numpy as np
+np.set_printoptions(threshold=np.nan)
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
+#---- matplotlib, my old favorite...
+import matplotlib
+from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set(style='ticks')
+#---- for Bokeh, a ploter
+import bokeh.plotting as bp
+from bokeh.layouts import gridplot, column
+from bokeh.models import LinearAxis, DataRange1d, HoverTool, BoxSelectTool
+TOOLS = ["pan,wheel_zoom,box_zoom,reset,crosshair, hover, box_select"]
+bp.output_notebook()
+#---- for color, style print in iPython
+from IPython.display import HTML, display
+import datetime
+import os
+#---- for colorful print
+from colorama import init
+init(autoreset=True)
+from colorama import Fore, Back, Style
+#---- for statistics
+from scipy import stats
+
+class LabelManager:
+    def __init__(self):
+        self.DATE = 'Date'
+        self.OPEN = 'Open'
+        self.HIGH = 'High'
+        self.LOW  = 'Low'
+        self.CLOSE = 'Close'
+        self.VOLUME = 'Volume'
+label = LabelManager()
+
+TOOLTIPS = [
+    (label.DATE, '$1'),
+    (label.OPEN, '$2'),
+]
+
+class StockCracker:
+    def __init__(self):
+        
+        self.label = LabelManager()
+        print("Initializing StockCracker instance")
+        
+    def readOneCsv_Yahoo(self, filePath):
+        data = pd.read_csv(filePath, encoding='UTF-8', header=0)
+        data[label.DATE] = [datetime.datetime.strptime(d, '%Y-%m-%d').date() for d in data[label.DATE]]
+        data[label.OPEN] = pd.to_numeric(data[label.OPEN], errors='coerce')
+        data[label.HIGH] = pd.to_numeric(data[label.HIGH], errors='coerce')
+        data[label.LOW] = pd.to_numeric(data[label.LOW], errors='coerce')
+        data[label.CLOSE] = pd.to_numeric(data[label.CLOSE], errors='coerce')
+        data[label.VOLUME] = pd.to_numeric(data[label.VOLUME], errors='coerce')
+        return data
+        
+    # read one csv file
+    def readOneCsv(self, filePath, isReversed):
+        '''
+        Obsolete, do NOT use this API
+        '''
+        data = pd.read_csv(filePath, encoding='shift-JIS', header=0)
+        if isReversed is True:
+            data = data.reindex(index=data.index[::-1])
+            data.index = range(len(data))
+        #print(Fore.YELLOW + data['日付'][0] + '~' + data['日付'][len(data)-1])
+        return data
+
+    # read one folder, recurssively
+    def readOneFolder(self, folderPath, isReversed=True):
+        '''
+        Obsolete, do NOT use this API
+        '''
+        for root, subdirs, files in os.walk(folderPath):
+            #print('----------------')
+            #print(Back.CYAN + Fore.RED + Style.BRIGHT + 
+            #      'In Folder:'+root)
+            #---- sorted() here sorts files 
+            for oneCsv in sorted(files):
+                #print(Back.CYAN +"    "+ oneCsv)
+                filePath = folderPath+oneCsv
+                data = self.readOneCsv(filePath, isReversed)
+                try:
+                    allData
+                except NameError:   # allData not exist
+                    allData = data
+                else:   # allData exist
+                    # concatenate 2 pandas dataframes
+                    allData = pd.concat([allData, data])
+        allData.index = range(len(allData))
+        #print(allData) # output all data of target stock
+        return allData
+
+    # calculate Beta coefficient, 
+    def calcBetaCoefficient(self, stock, index):
+        #---- 
+#### following example gives a good intuition about the quantitative  
+#        >>> a = np.array([1,3,9,6,5])
+#        >>> b = a + 100
+#        >>> b
+#        array([101, 103, 109, 106, 105])
+#        >>> np.cov(a,b,bias=1)
+#        array([[ 7.36,  7.36],
+#                [ 7.36,  7.36]])
+#        >>> b = a * 10
+#        >>> np.cov(a,b,bias=1)
+#        array([[   7.36,   73.6 ],
+#               [  73.6 ,  736.  ]])
+        # clean data, stock and index should have same date time before calculation
+        sharedDate = list(set(stock[self.label.DATE]).intersection(index[self.label.DATE]))
+        stock = stock[stock[self.label.DATE].isin(sharedDate)]
+        index = index[index[self.label.DATE].isin(sharedDate)]
+        # calculate daily change of stock and index
+        dailyChange_stock = np.array([(stock.iloc[idx+1][self.label.CLOSE] - stock.iloc[idx][self.label.CLOSE]) / stock.iloc[idx][self.label.CLOSE] for idx in range(len(stock) - 1)]) # from 0 ~ len(stock)-1
+        dailyChange_index = np.array([(index.iloc[idx+1][self.label.CLOSE] - index.iloc[idx][self.label.CLOSE]) / index.iloc[idx][self.label.CLOSE] for idx in range(len(index) - 1)]) # from 0 ~ len(index)-1
+        # covariance matrix(a, b) = 
+        # [[var(a)    cov(a, b)]
+        #  [cov(b, a) var(b)   ]]
+        #
+        matrix = np.cov(dailyChange_stock, dailyChange_index, bias=1)        
+        beta = matrix[0][1] / matrix[1][1]
+        print("Beta = {}".format(beta))
+        if beta > 1:
+            print("Target Stock is MORE volitale than the index, target stock is RISKY")
+        else:
+            print("Target Stock is LESS volitale than the index, target stock is NOT risky")
+        return beta, dailyChange_stock, dailyChange_index
+    
+    # On-balance volume, need 出来高 of the target stock
+    def keyIndicator_OBV(self, stock):
+        OBV = np.zeros(len(stock))
+        OBV[0] = 0 
+        for idx in range(len(stock)):
+            if idx == 0:
+                pass
+            if stock[self.label.CLOSE].iloc[idx] > stock[self.label.CLOSE].iloc[idx - 1]:
+                OBV[idx] = OBV[idx - 1] + stock[self.label.VOLUME].iloc[idx]
+            elif stock[self.label.CLOSE].iloc[idx] < stock[self.label.CLOSE].iloc[idx - 1]:
+                OBV[idx] = OBV[idx - 1] - stock[self.label.VOLUME].iloc[idx]
+            else:
+                OBV[idx] = OBV[idx - 1]
+        return OBV
+    #-- MACD
+    #----- Simple moving average
+    def calc_SMA(self, stock, N):
+        SMA = np.zeros(len(stock))
+        for i in range(len(stock)):
+            if i > N-2:
+                """Why N-2? 
+                e.g., if N=10, need to sum day0~day9, therefore need to begin from i=9 (aka.i>10-2=8)
+                """
+                SMA[i] = stock[self.label.CLOSE][i-N+1:i].sum() / N
+        return SMA
+    #----- Exponential moving average
+    def calc_EMA(self, stock, N):
+        SMA = self.calc_SMA(stock, N)
+        EMA = np.zeros(len(stock))
+        EMA[N-1] = SMA[N-1]             # EMA_0 value
+        multiplier = 2 / (N + 1)
+        for i in range(len(stock)):
+            if i > N - 1:
+                EMA[i] = multiplier * (stock[self.label.CLOSE].iloc[i] - EMA[i-1]) + EMA[i-1]
+        return EMA
+    #----- MACD and signal
+    def keyIndicator_MACD(self, stock, N1, N2, sigN):
+        EMA_N1 = self.calc_EMA(stock, N1)
+        EMA_N2 = self.calc_EMA(stock, N2)
+        MACD   = EMA_N1 - EMA_N2
+        #--- zerolize all necessary index
+        if N1 < N2:
+            border = N2 - 1
+        else:
+            border = N1 - 1
+        for idx in range(border):
+            MACD[idx] = 0
+        #--- calc Signal line
+        multiplier = 2/(sigN + 1)
+        signal = np.zeros(len(stock))
+        signal[border+sigN-1] = np.average( MACD[border:border+sigN-1] )
+        for idx in range(border+sigN, len(stock)):
+            signal[idx] = MACD[idx] * multiplier + signal[idx-1] * (1 - multiplier)
+        return MACD, signal
+    #-------------
+    # 我需要分析index以及之前N天，之后M天的数据～
+    #-------------
+    def focusOn(self, stock, index, formerN, laterM):
+        start = index - formerN
+        if start < 0:
+            start = 0
+            
+        end = index + laterM
+        if end > stock.size:
+            end = len(stock)
+        return stock[start:end]
+    #-------------
+    # 计算LinearRegression
+    #-------------
+    def calcSlope(self, data):
+        return
--- updateData.py
+++ updateData.py
@@ -14,123 +14,129 @@ import os
 
 #--------------- Stock Data -------------------------
 # target stock names, and save path
-tickers = {
-           #--- Index
-           '^N225':     './data/stock/nikkei/',             # Nikkei 225 Index
-           #--- Vehicle OEMs
-           '7261.T':    './data/stock/mazda/',              # Mazda, Nikkei
-           '7203.T':    './data/stock/toyota/',             # Toyota, Nikkei
-           '7201.T':    './data/stock/nissan/',             # Nissan, Nikkei
-           '7267.T':    './data/stock/honda/',              # Honda, Nikkei
-           #--- Entertainmen
-           '4680.T':    './data/stock/round1/',             # Round 1, Nikkei
-           '2412.T':    './data/stock/benefitone/',         # BenefitOne, Nikkei
-           '7494.T':    './data/stock/nintendo/',           # Nintendo, game company, Nikkei
-           #--- Consumer Electronics Maker
-           '6752.T':    './data/stock/panasonic/',          # Panasonic, Nikkei
-           '6758.T':    './data/stock/sony/',               # Sony, Nikkei
-           '3993.T':    './data/stock/pksha/',              # PKSHA, Toyota owned AI software company, Nikkei
-           '6501.T':    './data/stock/hitachi/',            # Hitachi, Nikkei
-           '7731.T':    './data/stock/nikon/',              # Nikon camera maker, Nikkei
-           '7751.T':    './data/stock/canon/',              # Canon camera maker, Nikkei
-           '6902.T':    './data/stock/denso/',              # Denso, Toyota's bitch, Nikkei
-           '6503.T':    './data/stock/mitsubishi-electric/',# Mitsubishi-electric, 
-           '6502.T':    './data/stock/toshiba/',            # Toshiba, seems they are coming back    
-           #--- Tech
-           '9984.T':    './data/stock/softbank/',           # Softbank, 
-           '9613.T':    './data/stock/nttdata/',            # NTT Data
-           '9432.T':    './data/stock/ntt/',                # NTT
-           '3807.T':    './data/stock/fisco/',              # Fisco, making investing tools, Nikkei
-           '4716.T':    './data/stock/oracle/',             # Oracle Japan, Nikkei
-           '4755.T':    './data/stock/rakuten/',            # Rakuten, 
-           '9433.T':    './data/stock/kddi/',               # KDDI, 
-           '3966.T':    './data/stock/uzabase/',            # UZABASE, news/fiance analyzor
-           '6146.T':    './data/stock/disco/',              # Disco, material/device maker
-           '3655.T':    './data/stock/brainpad/',           # BrainPad, some big data company
-           '4385.T':    './data/stock/mercari/',            # Mercari, a mobile app eCommerce company
-           '3092.T':    './data/stock/zozo/',               # ZOZO, eCommerce company
-           #--- Banking
-           '8411.T':    './data/stock/mizuho/',             # Mizuho Finance Group, Nikkei
-           '8306.T':    './data/stock/mitsubishi-ufj/',     # Mitsubishi UFJ
-           '8316.T':    './data/stock/smfg/',               # 住友 Finance Group
-           #--- Food & Drinks
-           '2579.T':    './data/stock/coca-cola/',          # Coca Cola Japan Holdings, 
-           '2587.T':    './data/stock/suntory/',            # Suntory
-           '2502.T':    './data/stock/asahi/',              # Asahi
-           #--- Service
-           '6098.T':    './data/stock/recruit/',            # Recruit
-           '6178.T':    './data/stock/jppost/',             # 日本郵政
-           '9603.T':    './data/stock/his/',                # H.I.S
-           #--- Retail
-           '8267.T':    './data/stock/aeon/',               # AEON, supermarket,
-           #--- Transportation
-           '9202.T':    './data/stock/ana/',                # ANA airline
-           '9201.T':    './data/stock/jal/',                # JAL airline
-           '9020.T':    './data/stock/jr-east/',            # East Japan Railway
-           '9021.T':    './data/stock/jr-west/',            # West Japan Railway
-           #--- Chemistry
-           '4911.T':    './data/stock/shiseido/',           # 資生堂
-           '4901.T':    './data/stock/fujifilm/',           # Fuji Film
-           '3402.T':    './data/stock/toray/',              # 東レ
-           #--- Trading 
-           '8001.T':    './data/stock/idochu/',             # 伊藤忠商事
-           #--- Insurance
-           '8766.T':    './data/stock/tokiomarine/',        # 東京海上日動
-           #--- Oil
-           '5020.T':    './data/stock/jxtg/',               # JXTG, 
-           #--- TV
-           '9413.T':    './data/stock/tv-tokyo/',           # TV Tokyo
-           '9409.T':    './data/stock/tv-asahi/',           # TV Asahi
-           #--- Media
-           '2371.T':    './data/stock/kakaku/',             # Kakaku.com
+class Tickers:
+    def __init__(self):
+        self.tickers = {
+                #--- Index
+                '^N225':     './data/stock/nikkei/',             # Nikkei 225 Index
+                #--- Vehicle OEMs
+                '7261.T':    './data/stock/mazda/',              # Mazda, Nikkei
+                '7203.T':    './data/stock/toyota/',             # Toyota, Nikkei
+                '7201.T':    './data/stock/nissan/',             # Nissan, Nikkei
+                '7267.T':    './data/stock/honda/',              # Honda, Nikkei
+                #--- Entertainmen
+                '4680.T':    './data/stock/round1/',             # Round 1, Nikkei
+                '2412.T':    './data/stock/benefitone/',         # BenefitOne, Nikkei
+                '7494.T':    './data/stock/nintendo/',           # Nintendo, game company, Nikkei
+                #--- Consumer Electronics Maker
+                '6752.T':    './data/stock/panasonic/',          # Panasonic, Nikkei
+                '6758.T':    './data/stock/sony/',               # Sony, Nikkei
+                '3993.T':    './data/stock/pksha/',              # PKSHA, Toyota owned AI software company, Nikkei
+                '6501.T':    './data/stock/hitachi/',            # Hitachi, Nikkei
+                '7731.T':    './data/stock/nikon/',              # Nikon camera maker, Nikkei
+                '7751.T':    './data/stock/canon/',              # Canon camera maker, Nikkei
+                '6902.T':    './data/stock/denso/',              # Denso, Toyota's bitch, Nikkei
+                '6503.T':    './data/stock/mitsubishi-electric/',# Mitsubishi-electric, 
+                '6502.T':    './data/stock/toshiba/',            # Toshiba, seems they are coming back    
+                #--- Tech
+                '9984.T':    './data/stock/softbank/',           # Softbank, 
+                '9613.T':    './data/stock/nttdata/',            # NTT Data
+                '9432.T':    './data/stock/ntt/',                # NTT
+                '3807.T':    './data/stock/fisco/',              # Fisco, making investing tools, Nikkei
+                '4716.T':    './data/stock/oracle/',             # Oracle Japan, Nikkei
+                '4755.T':    './data/stock/rakuten/',            # Rakuten, 
+                '9433.T':    './data/stock/kddi/',               # KDDI, 
+                '3966.T':    './data/stock/uzabase/',            # UZABASE, news/fiance analyzor
+                '6146.T':    './data/stock/disco/',              # Disco, material/device maker
+                '3655.T':    './data/stock/brainpad/',           # BrainPad, some big data company
+                '4385.T':    './data/stock/mercari/',            # Mercari, a mobile app eCommerce company
+                '3092.T':    './data/stock/zozo/',               # ZOZO, eCommerce company
+                #--- Banking
+                '8411.T':    './data/stock/mizuho/',             # Mizuho Finance Group, Nikkei
+                '8306.T':    './data/stock/mitsubishi-ufj/',     # Mitsubishi UFJ
+                '8316.T':    './data/stock/smfg/',               # 住友 Finance Group
+                #--- Food & Drinks
+                '2579.T':    './data/stock/coca-cola/',          # Coca Cola Japan Holdings, 
+                '2587.T':    './data/stock/suntory/',            # Suntory
+                '2502.T':    './data/stock/asahi/',              # Asahi
+                #--- Service
+                '6098.T':    './data/stock/recruit/',            # Recruit
+                '6178.T':    './data/stock/jppost/',             # 日本郵政
+                '9603.T':    './data/stock/his/',                # H.I.S
+                #--- Retail
+                '8267.T':    './data/stock/aeon/',               # AEON, supermarket,
+                #--- Transportation
+                '9202.T':    './data/stock/ana/',                # ANA airline
+                '9201.T':    './data/stock/jal/',                # JAL airline
+                '9020.T':    './data/stock/jr-east/',            # East Japan Railway
+                '9021.T':    './data/stock/jr-west/',            # West Japan Railway
+                #--- Chemistry
+                '4911.T':    './data/stock/shiseido/',           # 資生堂
+                '4901.T':    './data/stock/fujifilm/',           # Fuji Film
+                '3402.T':    './data/stock/toray/',              # 東レ
+                #--- Trading 
+                '8001.T':    './data/stock/idochu/',             # 伊藤忠商事
+                #--- Insurance
+                '8766.T':    './data/stock/tokiomarine/',        # 東京海上日動
+                #--- Oil
+                '5020.T':    './data/stock/jxtg/',               # JXTG, 
+                #--- TV
+                '9413.T':    './data/stock/tv-tokyo/',           # TV Tokyo
+                '9409.T':    './data/stock/tv-asahi/',           # TV Asahi
+                #--- Media
+                '2371.T':    './data/stock/kakaku/',             # Kakaku.com
 
-           }
+                }
 
-# set start, end date, begin download data
-yf.pdr_override()
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime.today()
 
-print('Downloading: Stock data')
-for key, value in tickers.items():
+if __name__ == '__main__':
+    t = Tickers()
+
+    # set start, end date, begin download data
+    yf.pdr_override()
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime.today()
+
+    print('Downloading: Stock data')
+    for key, value in t.tickers.items():
+        try:
+            print('Getting: ' + key + ' | ' + value[13:-1])
+            data = pdr.get_data_yahoo(key, start=start, end=end)
+            #print(data)
+        except:
+            print('Failed to download data')
+            os.system('pause')
+            exit(1)
+
+        os.makedirs(value, exist_ok=True)
+        data.to_csv(value + 'data.csv', encoding='utf-8')
+
+    #--------------- FX Data -------------------------
+    import httplib2
+    import sys
+
+    print('Downloading: FX data')
+    # get FX data, notice several parameters should be set or resp will be 404
     try:
-        print('Getting: ' + key + ' | ' + value[13:-1])
-        data = pdr.get_data_yahoo(key, start=start, end=end)
-        #print(data)
+        h = httplib2.Http()    
+        resp, content = h.request("http://www.m2j.co.jp/market/pchistry_dl.php?ccy=1&type=d", "GET", 
+                                headers={"accept-encoding":"gzip, deflate", "accept-language":"zh,ja;q=0.8,en-US;q=0.6,en;q=0.4", "referer":"http://www.m2j.co.jp/market/historical.php"})
     except:
-        print('Failed to download data')
-        os.system('pause')
-        exit(1)
+        print("Using Proxy")
+        h = httplib2.Http(
+            proxy_info = httplib2.ProxyInfo(
+                httplib2.socks.PROXY_TYPE_HTTP, 'proxy.mei.co.jp', 8080
+                ) )
+        resp, content = h.request("http://www.m2j.co.jp/market/pchistry_dl.php?ccy=1&type=m", "GET")
 
-    os.makedirs(value, exist_ok=True)
-    data.to_csv(value + 'data.csv', encoding='utf-8')
+    path = './data/FX/USDJPY/'
+    os.makedirs(path, exist_ok=True)
 
-#--------------- FX Data -------------------------
-import httplib2
-import sys
+    oFile = open(path + 'data.csv', 'wb')
+    oFile.write(content)
+    oFile.close()
 
-print('Downloading: FX data')
-# get FX data, notice several parameters should be set or resp will be 404
-try:
-    h = httplib2.Http()    
-    resp, content = h.request("http://www.m2j.co.jp/market/pchistry_dl.php?ccy=1&type=d", "GET", 
-                              headers={"accept-encoding":"gzip, deflate", "accept-language":"zh,ja;q=0.8,en-US;q=0.6,en;q=0.4", "referer":"http://www.m2j.co.jp/market/historical.php"})
-except:
-    print("Using Proxy")
-    h = httplib2.Http(
-        proxy_info = httplib2.ProxyInfo(
-            httplib2.socks.PROXY_TYPE_HTTP, 'proxy.mei.co.jp', 8080
-            ) )
-    resp, content = h.request("http://www.m2j.co.jp/market/pchistry_dl.php?ccy=1&type=m", "GET")
-
-path = './data/FX/USDJPY/'
-os.makedirs(path, exist_ok=True)
-
-oFile = open(path + 'data.csv', 'wb')
-oFile.write(content)
-oFile.close()
-
-print('Downloading: BitCoin data')
-#--------------- BitCoin Data -------------------------
-from BTCUpdate import updateCryptoCurrencyData
-updateCryptoCurrencyData()
+    print('Downloading: BitCoin data')
+    #--------------- BitCoin Data -------------------------
+    from BTCUpdate import updateCryptoCurrencyData
+    updateCryptoCurrencyData()
