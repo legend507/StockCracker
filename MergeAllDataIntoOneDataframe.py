ca9f2610a270fd1414320ccf8b16eeee6e9670c8
CHEN <chen@ubuntu.ubuntu-domain>
2019-06-02 17:55:21 +0900

    Finished merging all stock dataframes into one dataframe. (TODO: build ML model) Update data. (FX data has error... Seems the source site is fsck-ed...)

--- MergeAllDataIntoOneDataframe.py
+++ MergeAllDataIntoOneDataframe.py
@@ -2,11 +2,23 @@ from updateData import Tickers
 from StockCracker import StockCracker
 from StockCracker import LabelManager
 
+import pandas as pd
+
 allStock = Tickers()
 sc = StockCracker()
 
+# create empty dataframe to hold all data
+agg_df = pd.DataFrame()
+
+# traverse all tickers defined in updateData.py, and merge the dfs into one df
 for name, value in allStock.tickers.items():
     print('{}: {}'.format(name, value))
     df = sc.readOneCsv_Yahoo(value + 'data.csv')
-    df.columns = [value[13:-1] + '-' + x for x in df.columns ]
-    print(df.head())
+    df.columns = ['Date'] + [value[13:-1] + '-' + x for x in df.columns if x != 'Date']
+    if (agg_df.empty):
+        agg_df = df
+    else:
+        # by specifing how='outer', the aggregated df keeps rows with no values
+        agg_df = pd.merge(agg_df, df, on='Date', how='outer')
+    
+agg_df.to_csv('./data/stock_aggregated.csv')
